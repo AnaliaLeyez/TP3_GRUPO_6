1ad8ce2c1c6e83533c0b3e6fe085cb7a18d94e47
BILBODEVE <facundotaraujo@hotmail.com>
2025-04-13 15:58:13 -0300

    prueba de metodo leer()

--- src/ejercicio1/Principal.java
+++ src/ejercicio1/Principal.java
@@ -1,13 +1,18 @@
 package ejercicio1;
 
+import java.util.TreeSet;
 
 public class Principal {
 
 	public static void main(String[] args) {
 
-		Archivo personas= new Archivo("src/ejercicio1/Personas.txt");
-		personas.leePersonas();
+		Archivo filePersonas= new Archivo("src/ejercicio1/Personas.txt");
+		TreeSet<Persona> personas= new TreeSet<Persona>();
+		personas = filePersonas.leer();
 		
+		for(Persona p : personas)
+		{
+			System.out.println(p.toString());
+		}
 	}
-
 }
